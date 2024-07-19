82af4163b30088d1ececa3f86cd12fa7b8e55aab
sam650095 <sam00660011@gmail.com>
2024-07-19 18:18:16 +0800

    完成udentify the gate 這一頁的修改表單前端部分

--- base/static/base/js/identifythegates.js
+++ base/static/base/js/identifythegates.js
@@ -1,3 +1,4 @@
+let editdata = [];
 document.addEventListener("DOMContentLoaded", async function () {
   const csrftoken = getCookie("csrftoken");
   const preload_results = await fetchAPI(
@@ -49,6 +50,8 @@ async function processbtn(event) {
   toggleLoading(false, "processbutton");
 
   document.getElementById("btnbox").classList.remove("hidden");
+  document.getElementById("clearbtn").classList.remove("hidden");
+  document.getElementById("confirmbtn").classList.remove("hidden");
   document.getElementById("nextbtn").classList.remove("hidden");
 }
 function createTable(data) {
@@ -66,10 +69,12 @@ function createTable(data) {
     th.className = "px-6 py-3";
     thead.appendChild(th);
   });
+  console.log(data);
+  editedData = JSON.parse(JSON.stringify(data));
 
   data.forEach((row, index) => {
     const tr = document.createElement("tr");
-    tr.className = "bg-white border-b dark:bg-gray-800 dark:border-gray-700";
+    tr.className = "bg-white border-b hover:bg-gray-50 cursor-pointer";
 
     const tdIndex = document.createElement("td");
     tdIndex.textContent = index + 1;
@@ -79,10 +84,35 @@ function createTable(data) {
     Object.values(row).forEach((value) => {
       const td = document.createElement("td");
       td.textContent = value !== null ? value : "N/A";
-      td.className = "px-6 py-4";
+      td.className = "w-1/3 px-6 py-4";
+      td.onclick = () => handleClick(td, index);
       tr.appendChild(td);
     });
 
     tbody.appendChild(tr);
   });
+}
+function handleClick(td, index) {
+  if (td.querySelector("input")) {
+    return;
+  }
+  const div = document.createElement("div");
+  div.className = "flex justify-between";
+  const currentValue = td.textContent;
+  td.textContent = "";
+  const input = document.createElement("input");
+  input.type = "text";
+  input.value = currentValue;
+  input.className =
+    "my-2 w-36 text-sm text-gray-900 border border-gray-300 rounded-lg bg-gray-50 focus:ring-sky-900 focus:border-sky-900";
+
+  div.appendChild(input);
+  const btn = document.createElement("button");
+  btn.textContent = "confirm";
+  btn.className =
+    "rounded-md bg-sky-700 py-2 px-4 text-white text-sm font-semibold my-3";
+
+  div.appendChild(btn);
+  td.appendChild(div);
+  input.focus();
 }
